f4fb7286edc48906549a85617d87f8b6909103b9
gagatek1 <jakub.gagat@outlook.com>
2024-10-20 13:27:46 +0200

    Edit update user function

--- app/api/v1/endpoints/user_endpoint.py
+++ app/api/v1/endpoints/user_endpoint.py
@@ -4,7 +4,7 @@ from passlib.context import CryptContext
 
 from starlette import status
 
-from app.schema.user import CreateUser
+from app.schema.user import CreateUser, UpdateUser
 from app.models.user import User
 from app.core.database import db_dependency
 
@@ -27,7 +27,7 @@ async def create_user(db: db_dependency, create_user: CreateUser):
 
     return { 'status': 'created' }
 
-@user_router.put('/update/{user_id}')
+@user_router.put('/update/{user_id}', status_code=status.HTTP_202_ACCEPTED)
 async def update_user(user_id: int, db: db_dependency, update_user: UpdateUser):
     db_user = db.query(User).filter(User.id == user_id).first()
 
@@ -36,7 +36,7 @@ async def update_user(user_id: int, db: db_dependency, update_user: UpdateUser):
     
     db_user.email = update_user.email
     db_user.username = update_user.username
-    db_user.hashed_password = update_user.password
+    db_user.hashed_password = bcrypt_context.hash(update_user.password)
     db.commit()
     db.refresh(db_user)
 
