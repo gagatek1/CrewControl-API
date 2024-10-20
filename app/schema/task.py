63c53ec9e958c963221f5e5f38810a2cc5080bf3
gagatek1 <jakub.gagat@outlook.com>
2024-10-20 23:33:03 +0200

    Add admin create task class

--- app/schema/task.py
+++ app/schema/task.py
@@ -5,6 +5,11 @@ class CreateTask(BaseModel):
     name: str
     description: str
 
+class AdminCreateTask(BaseModel):
+    name: str
+    description: str
+    user_id: int
+
 class UpdateTask(BaseModel):
     name: Optional[str] = None
     description: Optional[str] = None
