8d492ddcfd1f5e8fb48812c32de5a1b2e13440f1
gagatek1 <jakub.gagat@outlook.com>
2024-10-20 14:39:53 +0200

    Edit main file

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
 
 from app.api.v1.router import router
+from app.core.database import Base, engine
 
 app = FastAPI()
 
 app.include_router(router)
+
+Base.metadata.create_all(bind=engine)
