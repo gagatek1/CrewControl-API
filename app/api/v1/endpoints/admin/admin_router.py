a3f9fe18b1ab3736f911f7b7cd83e57eb84338e1
gagatek1 <jakub.gagat@outlook.com>
2024-10-20 23:24:18 +0200

    Include admin task router

--- app/api/v1/endpoints/admin/admin_router.py
+++ app/api/v1/endpoints/admin/admin_router.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter
 
 from app.api.v1.endpoints.admin.admin_user_endpoint import admin_user_router
+from app.api.v1.endpoints.admin.admin_task_endpoint import admin_task_router
 
 admin_router = APIRouter(prefix='/admin', tags=['admin'])
 
 admin_router.include_router(admin_user_router)
+admin_router.include_router(admin_task_router)
 
