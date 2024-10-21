5d3010ff00c37557cf63f8cdebb29b4000c28e40
gagatek1 <jakub.gagat@outlook.com>
2024-10-21 14:04:09 +0200

    Edit schema

--- app/schema/team.py
+++ app/schema/team.py
@@ -1,5 +1,12 @@
 from pydantic import BaseModel
 
+from typing import Optional
+
 class CreateTeam(BaseModel):
     name: str
     department: str
+
+class UpdateTeam(BaseModel):
+    name: Optional[str] = None
+    department: Optional[str] = None
+    team_leader: Optional[int] = None
