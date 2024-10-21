f028d68218b2f70756a7e05a01d2e0fd5a883040
gagatek1 <jakub.gagat@outlook.com>
2024-10-21 20:31:52 +0200

    Edit create endpoint

--- app/api/v1/endpoints/team_endpoint.py
+++ app/api/v1/endpoints/team_endpoint.py
@@ -1,27 +1,18 @@
 from fastapi import APIRouter, Depends, HTTPException
 
 from app.models.team import Team
-from app.models.user import User
 from app.schema.team import CreateTeam, UpdateTeam
 from app.core.database import db_dependency
 from app.core.security import get_current_user
+from app.services.team.create_service import create_service
 
 team_router = APIRouter(prefix='/teams', tags=['teams'])
 
 @team_router.post('/create')
 async def create_team(db: db_dependency, create_team: CreateTeam, get_user: dict = Depends(get_current_user)):
-    user = db.query(User).filter(User.id == get_user['user_id']).first()
+    user = create_service(get_user, create_team, db)
 
-    create_team_model = Team(
-        name=create_team.name,
-        department=create_team.department,
-        team_leader=user.id
-    )
-
-    db.add(create_team_model)
-    db.commit()
-
-    return { 'status': 'created' }
+    return user
 
 @team_router.get('/')
 async def show_teams(db: db_dependency, get_user: dict = Depends(get_current_user)):
