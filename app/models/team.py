bd0e23063e21cb9adc319ef79a1e84b68241424a
gagatek1 <jakub.gagat@outlook.com>
2024-10-21 13:26:10 +0200

    Add tablename

--- app/models/team.py
+++ app/models/team.py
@@ -4,6 +4,8 @@ from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
 
 class Team(Base):
+    __tablename__ = 'teams'
+
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, index=True)
     department = Column(String, index=True)
