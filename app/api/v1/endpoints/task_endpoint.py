517a068a2f38a0a8e26cf36a054d8e94e87e5fbb
gagatek1 <jakub.gagat@outlook.com>
2024-10-21 21:04:00 +0200

    Edit update endpoint

--- app/api/v1/endpoints/task_endpoint.py
+++ app/api/v1/endpoints/task_endpoint.py
@@ -4,6 +4,7 @@ from app.core.database import db_dependency
 from app.core.security import get_current_user
 from app.schema.task import CreateTask, UpdateTask
 from app.models.task import Task
+from app.services.task.update_service import update_service
 from app.services.task.create_service import create_service
 
 task_router = APIRouter(prefix='/tasks', tags=['tasks'])
@@ -33,21 +34,9 @@ async def get_task(task_id: int, db: db_dependency, get_user: dict = Depends(get
     
 @task_router.put('/update/{task_id}')
 async def update_task(task_id: int, db: db_dependency, update_task: UpdateTask, get_user: dict = Depends(get_current_user)):
-    task = db.query(Task).filter(Task.id == task_id).first()
-    
-    if task is None:
-        raise HTTPException(status_code=404, detail='Task not found')
-    if task.user_id == get_user['user_id']:
-        if update_task.name is not None: task.name = update_task.name
-        if update_task.description is not None: task.description = update_task.description
-        if update_task.done is not None: task.done = update_task.done
+    task = update_service(task_id, get_user, update_task, db)
 
-        db.commit()
-        db.refresh(task)
-
-        return task
-    else:
-        raise HTTPException(status_code=401)
+    return task
     
 @task_router.delete('/delete/{task_id}')
 async def delete_task(task_id: int, db: db_dependency, get_user: dict = Depends(get_current_user)):
