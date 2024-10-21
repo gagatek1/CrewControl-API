ab0d00be4f825dcd113a4c28788b95112816fb48
gagatek1 <jakub.gagat@outlook.com>
2024-10-21 20:38:14 +0200

    Edit update endpoint

--- app/api/v1/endpoints/team_endpoint.py
+++ app/api/v1/endpoints/team_endpoint.py
@@ -5,6 +5,7 @@ from app.schema.team import CreateTeam, UpdateTeam
 from app.core.database import db_dependency
 from app.core.security import get_current_user
 from app.services.team.create_service import create_service
+from app.services.team.update_service import update_service
 
 team_router = APIRouter(prefix='/teams', tags=['teams'])
 
@@ -31,21 +32,9 @@ async def get_team(team_id: int, db: db_dependency, get_user: dict = Depends(get
 
 @team_router.put('/update/{team_id}')
 async def update_team(team_id: int, db: db_dependency, update_team: UpdateTeam, get_user: dict = Depends(get_current_user)):
-    team = db.query(Team).filter(Team.id == team_id).first()
-    user_id = get_user['user_id']
+    team = update_service(team_id, update_team, get_user, db)
 
-    if team is None:
-        raise HTTPException(status_code=404, detail='Could not find team')
-    if team.team_leader == user_id:
-        if update_team.name is not None: team.name = update_team.name
-        if update_team.department is not None: team.department = update_team.department
-        if update_team.team_leader is not None: team.team_leader = update_team.team_leader
-
-        db.commit()
-        db.refresh(team)
-
-        return team
-    raise HTTPException(status_code=401)
+    return team
 
 @team_router.delete('/delete/{team_id}')
 async def delete_team(team_id: int, db: db_dependency, get_user: dict = Depends(get_current_user)):
