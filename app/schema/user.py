ac5ad2ed49d044033162ad3195be513bba95fc8c
gagatek1 <jakub.gagat@outlook.com>
2024-10-22 23:37:07 +0200

    Edit schema

--- app/schema/user.py
+++ app/schema/user.py
@@ -17,11 +17,11 @@ class UpdateUser(BaseModel):
     password: Optional[str] = None
 
 class AdminUpdateUser(BaseModel):
-    username: str
-    email: EmailStr
-    first_name: str
-    last_name: str
-    role: UserRole
+    username: Optional[str] = None
+    email: Optional[EmailStr] = None
+    first_name: Optional[str] = None
+    last_name: Optional[str] = None
+    role: Optional[UserRole]= UserRole.user
 
 class JoinUser(BaseModel):
     team_id: int
