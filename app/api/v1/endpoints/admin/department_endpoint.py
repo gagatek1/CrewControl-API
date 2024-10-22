71073024bff2acfa71cc47c6eea9422517ebf21d
gagatek1 <jakub.gagat@outlook.com>
2024-10-23 01:22:59 +0200

    Add delete endpoint

--- app/api/v1/endpoints/admin/department_endpoint.py
+++ app/api/v1/endpoints/admin/department_endpoint.py
@@ -8,6 +8,7 @@ from app.models.user import User
 from app.schema.department import Department
 from app.services.admin.department.create_service import create_service
 from app.services.admin.department.update_service import update_service
+from app.services.admin.department.delete_service import delete_service
 
 department_router = APIRouter(prefix='/departments')
 
@@ -24,3 +25,9 @@ async def update_department(department_id, update_department: Department, db: db
     department = update_service(current_admin, department_id, update_department, db)
 
     return department
+
+@department_router.delete('/delete/{department_id}', status_code=status.HTTP_204_NO_CONTENT)
+async def delete_department(department_id, db: db_dependency, get_admin: dict = Depends(get_current_user)):
+    current_admin = db.query(User).filter(User.id == get_admin['user_id']).first()
+
+    delete_service(current_admin, department_id, db)
