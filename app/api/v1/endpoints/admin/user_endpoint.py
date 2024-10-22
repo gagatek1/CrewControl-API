46348dd3c44e532b5e0e822a59ff32a4000c369d
gagatek1 <jakub.gagat@outlook.com>
2024-10-22 23:38:03 +0200

    Edit update endpoint

--- app/api/v1/endpoints/admin/user_endpoint.py
+++ app/api/v1/endpoints/admin/user_endpoint.py
@@ -4,6 +4,7 @@ from app.core.database import db_dependency
 from app.core.security import get_current_user
 from app.models.user import User, UserRole
 from app.schema.user import AdminUpdateUser
+from app.services.admin.user.update_service import update_service
 
 user_router = APIRouter(prefix='/users')
 
@@ -22,24 +23,10 @@ async def get_users(db: db_dependency, get_admin: dict = Depends(get_current_use
 @user_router.put('/update/{user_id}')
 async def update_user(user_id: int, db: db_dependency, update_user: AdminUpdateUser, get_admin: dict = Depends(get_current_user)):
     current_admin = db.query(User).filter(User.id == get_admin['user_id']).first()
-    user = db.query(User).filter(User.id == user_id).first()
+    user = update_service(current_admin, user_id, update_user, db)
 
-    if not user:
-        raise HTTPException(status_code=404, detail='Could not find user')
-
-    if current_admin.role == UserRole.admin:
-        user.email = update_user.email
-        user.username = update_user.username
-        user.first_name = update_user.first_name
-        user.last_name = update_user.last_name
-        user.role = update_user.role
-
-        db.commit()
-        db.refresh(user)
-
-        return user
-    else:
-        raise HTTPException(status_code=401, detail='Could not validate')
+    return user
+    
 
 @user_router.delete('/delete/{user_id}')
 async def delete_user(user_id: int, db: db_dependency, get_admin: dict = Depends(get_current_user)):
