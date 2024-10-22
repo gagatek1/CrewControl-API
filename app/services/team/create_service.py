da1c0c9d2454c3d8770a921cbcd1a5daa0632566
gagatek1 <jakub.gagat@outlook.com>
2024-10-23 00:43:30 +0200

    Edit service

--- app/services/team/create_service.py
+++ app/services/team/create_service.py
@@ -1,12 +1,19 @@
+from fastapi import HTTPException
+
 from app.models.user import User
 from app.models.team import Team
+from app.models.department import Department
 
 def create_service(get_user, create_team, db):
     user = db.query(User).filter(User.id == get_user['user_id']).first()
+    department = db.query(Department).filter(Department.id == create_team.department_id).first()
+
+    if not department:
+        raise HTTPException(status_code=404, detail='Could not find department')
 
     create_team_model = Team(
         name=create_team.name,
-        department=create_team.department,
+        department_id=create_team.department_id,
         team_leader=user.id
     )
 
--- app/services/team/update_service.py
+++ app/services/team/update_service.py
@@ -1,19 +1,23 @@
 from fastapi import HTTPException
 
 from app.models.team import Team
+from app.models.department import Department
 
 def update_service(team_id, update_team, get_user, db):
     team = db.query(Team).filter(Team.id == team_id).first()
+    department = db.query(Department).filter(Department.id == update_team.department_id).first()
     user_id = get_user['user_id']
 
-    if team is None:
+    if not team:
         raise HTTPException(status_code=404, detail='Could not find team')
+    if not department:
+        raise HTTPException(status_code=404, detail='Could not find department')
     if team.team_leader == user_id:
         if update_team.name is not None:
             team.name = update_team.name
 
-        if update_team.department is not None:
-            team.department = update_team.department
+        if update_team.department_id is not None:
+            team.department_id = update_team.department_id
 
         if update_team.team_leader is not None:
             team.team_leader = update_team.team_leader
