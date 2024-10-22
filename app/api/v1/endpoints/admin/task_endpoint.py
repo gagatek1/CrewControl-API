a3a3cb60a4897a3423b09616897943b553784e12
gagatek1 <jakub.gagat@outlook.com>
2024-10-22 23:18:55 +0200

    Edit delete endpoint

--- app/api/v1/endpoints/admin/task_endpoint.py
+++ app/api/v1/endpoints/admin/task_endpoint.py
@@ -1,11 +1,14 @@
 from fastapi import APIRouter, Depends, HTTPException
 
+from starlette import status
+
 from app.core.database import db_dependency
 from app.core.security import get_current_user
 from app.models.task import Task
 from app.models.user import UserRole, User
 from app.schema.task import UpdateTask, AdminCreateTask
 from app.services.admin.task.update_service import update_service
+from app.services.admin.task.delete_service import delete_service
 
 task_router = APIRouter(prefix='/tasks')
 
@@ -26,20 +29,12 @@ async def update_task(task_id: int, db: db_dependency, update_task: UpdateTask, 
 
     return task
 
-@task_router.delete('/delete/{task_id}')
+@task_router.delete('/delete/{task_id}', status_code=status.HTTP_204_NO_CONTENT)
 async def delete_task(task_id: int, db: db_dependency, get_admin: dict = Depends(get_current_user)):
     current_admin = db.query(User).filter(User.id == get_admin['user_id']).first()
-    task = db.query(Task).filter(Task.id == task_id).first()
 
-    if current_admin.role == UserRole.admin:
-        if not task:
-            raise HTTPException(status_code=404, detail='Could not find task')
-        db.delete(task)
-        db.commit()
-
-        return { 'status': 'deleted' }
-    else:
-        raise HTTPException(status_code=401, detail='Could not validate')
+    delete_service(current_admin, task_id, db)
+    
     
 @task_router.get('/{task_id}')
 async def show_task(task_id: int, db: db_dependency, get_admin: dict = Depends(get_current_user)):
