18dedfd870895be18bc4494fdd1289e55c92d9a1
gagatek1 <jakub.gagat@outlook.com>
2024-10-22 23:43:26 +0200

    Edit delete endpoint

--- app/api/v1/endpoints/admin/user_endpoint.py
+++ app/api/v1/endpoints/admin/user_endpoint.py
@@ -1,10 +1,13 @@
 from fastapi import APIRouter, Depends, HTTPException
 
+from starlette import status
+
 from app.core.database import db_dependency
 from app.core.security import get_current_user
 from app.models.user import User, UserRole
 from app.schema.user import AdminUpdateUser
 from app.services.admin.user.update_service import update_service
+from app.services.admin.user.delete_service import delete_service
 
 user_router = APIRouter(prefix='/users')
 
@@ -28,17 +31,8 @@ async def update_user(user_id: int, db: db_dependency, update_user: AdminUpdateU
     return user
     
 
-@user_router.delete('/delete/{user_id}')
+@user_router.delete('/delete/{user_id}', status_code=status.HTTP_204_NO_CONTENT)
 async def delete_user(user_id: int, db: db_dependency, get_admin: dict = Depends(get_current_user)):
     current_admin = db.query(User).filter(User.id == get_admin['user_id']).first()
-    user = db.query(User).filter(User.id == user_id).first()
-
-    if current_admin.role == UserRole.admin:
-        if not user:
-            raise HTTPException(status_code=404, detail='Could not find user')
-        db.delete(user)
-        db.commit()
-
-        return { 'status': 'deleted' }
-    else:
-        raise HTTPException(status_code=401, detail='Could not validate')
+    
+    delete_service(current_admin, user_id, db)
