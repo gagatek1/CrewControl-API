b220cc680b3d6a1069b1d666520ec66d14497cad
gagatek1 <jakub.gagat@outlook.com>
2024-10-22 23:49:17 +0200

    Add update service

---
+++ app/services/admin/team/update_service.py
@@ -0,0 +1,23 @@
+from fastapi import HTTPException
+
+from app.models.team import Team
+from app.models.user import UserRole
+
+def update_service(current_admin, team_id, update_team, db):
+    team = db.query(Team).filter(Team.id == team_id).first()
+
+    if team is None:
+        raise HTTPException(status_code=404, detail='Could not find team')
+    if current_admin.role == UserRole.admin:
+        if update_team.name is not None: 
+            team.name = update_team.name
+        if update_team.department is not None: 
+            team.department = update_team.department
+        if update_team.team_leader is not None: 
+            team.team_leader = update_team.team_leader
+
+        db.commit()
+        db.refresh(team)
+
+        return team
+    raise HTTPException(status_code=401, detail='Could not validate')
