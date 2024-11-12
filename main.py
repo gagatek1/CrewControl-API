093ac3f0e8855652d06434259b18fbcbfcd2bb12
gagatek1 <jakub.gagat@outlook.com>
2024-11-12 02:34:37 +0100

    Add app name

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ from fastapi import FastAPI
 from app.api.v1.router import router
 from app.core.database import Base, engine
 
-app = FastAPI()
+app = FastAPI(
+    title='CrewControl',
+    version="0.5.0"
+)
 
 app.include_router(router)
 
